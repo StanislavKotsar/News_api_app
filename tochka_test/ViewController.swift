938f813e8e8c0fda97e2241264c892d3a3a23195
Stanislav Kotsar <minolin27@gmail.com>
2019-07-28 20:04:30 +0300

    Initial Commit

---
+++ tochka_test/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  tochka_test
+//
+//  Created by Станислав Коцарь on 28/07/2019.
+//  Copyright © 2019 Станислав Коцарь. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
